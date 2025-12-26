3d9312d91ceeaa0c1489d4aad366083d865cd9d6
aicha kh <aichakhorchani47@gmail.com>
2025-12-26 20:48:28 +0100

    fixed weekly forcast binding issue

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -8,7 +8,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WeatherDashboard.ViewModels;
 
 namespace WeatherDashboard.Views
 {
@@ -20,18 +19,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        var vm = new MainViewModel();
-            DataContext = vm;
-
-            // Subscribe to property changed for WeeklyForecast updates
-            vm.PropertyChanged += (s, e) =>
-            {
-                if (e.PropertyName == nameof(vm.WeeklyForecast))
-                {
-                    // Set DataContext of WeeklyForecastControl when WeeklyForecast updates
-                    WeeklyForecastControl.DataContext = new WeeklyForecastViewModel(vm.WeeklyForecast);
-                }
-        };
+        
     }
 }
 }
